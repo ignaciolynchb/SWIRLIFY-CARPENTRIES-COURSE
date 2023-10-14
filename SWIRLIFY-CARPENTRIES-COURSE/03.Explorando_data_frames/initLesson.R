f55faf77a658039e962b89f0c8dbca1207e673af
ignaciolynchb <ignaciolynchb@gmail.com>
2023-10-14 19:58:33 -0300

    Lecciones 1 a 6

---
+++ SWIRLIFY-CARPENTRIES-COURSE/03.Explorando_data_frames/initLesson.R
@@ -0,0 +1,9 @@
+# prender el log
+swirl_options(swirl_logging = TRUE)
+
+gatos <- data.frame(color = c("mixto", "negro", "atigrado"),
+                    peso = c(2.1, 5.0, 3.2),
+                    le_gusta_cuerda = c(1, 0, 1))
+# Path to data
+datapath <- file.path(path.package('swirl'), 'Courses',
+                       'SWIRLIFY CARPENTRIES COURSE', '03.Explorando_data_frames','gapminder-FiveYearData.csv')
