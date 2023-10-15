88c289b1a238554a4c049b3b5fd7b70f6a2546e3
ignaciolynchb <ignaciolynchb@gmail.com>
2023-10-15 10:02:22 -0300

    Leccion 5

---
+++ 5.Control_de_flujo/mi_primer_if.R
@@ -0,0 +1,17 @@
+# La estructura básica de un flujo de control es la siguiente:
+
+#if (la condición es verdad) {
+  #realizar una acción
+#} else {  # es decir, si la condición es falsa,
+  #realizar una acción alternativa
+#}
+
+# Ahora, pongamoslo a prueba.
+
+#Reemplace "#condicion" por la condición apropiada.
+
+x <- 8
+
+if (#condicion) {
+  print("x es mayor o igual que 10")
+}
