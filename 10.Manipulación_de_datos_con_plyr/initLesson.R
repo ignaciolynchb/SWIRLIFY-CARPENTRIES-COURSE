c60e41d9d27707397d1f5187ce507f47dee03dfd
mauriciogq1 <143091430+mauriciogq1@users.noreply.github.com>
2024-03-09 20:51:00 -0300

    Add files via upload

---
+++ 10.Manipulación_de_datos_con_plyr/initLesson.R
@@ -0,0 +1,18 @@
+# Code placed in this file fill be executed every time the
+      # lesson is started. Any variables created here will show up in
+      # the user's working directory and thus be accessible to them
+      # throughout the lesson.
+library(plyr)
+library(gapminder)
+calcGDP <- function(dat, year=NULL, country=NULL) {
+  if(!is.null(year)) {
+    dat <- dat[dat$year %in% year, ]
+  }
+  if (!is.null(country)) {
+    dat <- dat[dat$country %in% country,]
+  }
+  gdp <- dat$pop * dat$gdpPercap
+  
+  new <- cbind(dat, gdp=gdp)
+  return(new)
+}
