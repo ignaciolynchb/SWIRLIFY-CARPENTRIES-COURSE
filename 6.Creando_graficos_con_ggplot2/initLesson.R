08ff3724dbe716f729523c961df4ea842b593c08
mauriciogq1 <143091430+mauriciogq1@users.noreply.github.com>
2024-03-10 23:06:08 -0300

    Update initLesson.R

--- 6.Creando_graficos_con_ggplot2/initLesson.R
+++ 6.Creando_graficos_con_ggplot2/initLesson.R
@@ -3,3 +3,18 @@
       # the user's working directory and thus be accessible to them
       # throughout the lesson.
 library(ggplot2)
+swirl_options(swirl_logging = TRUE)
+library(googlesheets4)
+
+# Set authentication token to be stored in a folder called `.secrets`
+options(gargle_oauth_cache = ".secrets")
+
+# Authenticate manually
+gs4_auth()
+
+# If successful, the previous step stores a token file.
+# Check that a file has been created with:
+list.files(".secrets/")
+
+# Check that the non-interactive authentication works by first deauthorizing:
+gs4_deauth()
