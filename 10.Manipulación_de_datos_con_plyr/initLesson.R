63c9da8a53a337f41cb3336bef013c81fc06ee8b
mauriciogq1 <143091430+mauriciogq1@users.noreply.github.com>
2024-03-10 23:04:38 -0300

    Update initLesson.R

--- 10.Manipulación_de_datos_con_plyr/initLesson.R
+++ 10.Manipulación_de_datos_con_plyr/initLesson.R
@@ -16,3 +16,18 @@ calcGDP <- function(dat, year=NULL, country=NULL) {
   new <- cbind(dat, gdp=gdp)
   return(new)
 }
+swirl_options(swirl_logging = TRUE)
+library(googlesheets4)
+
+# Set authentication token to be stored in a folder called `.secrets`
+options(gargle_oauth_cache = ".secrets")
+
+# Authenticate manually
+gs4_auth()
+
+# If successful, the previous step stores a token file.
+# Check that a file has been created with:
+list.files(".secrets/")
+
+# Check that the non-interactive authentication works by first deauthorizing:
+gs4_deauth()
