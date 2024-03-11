5a20a7c5f5834a8ae57d872b468f2e972f56a66f
mauriciogq1 <143091430+mauriciogq1@users.noreply.github.com>
2024-03-10 23:07:47 -0300

    Update initLesson.R

--- 3.Explorando_data_frames/initLesson.R
+++ 3.Explorando_data_frames/initLesson.R
@@ -7,3 +7,18 @@ gatos <- data.frame(color = c("mixto", "negro", "atigrado"),
 # Path to data
 datapath <- file.path(path.package('swirl'), 'Courses',
                        'SWIRLIFY CARPENTRIES COURSE', '03.Explorando_data_frames','gapminder-FiveYearData.csv')
+library(googlesheets4)
+
+# Set authentication token to be stored in a folder called `.secrets`
+options(gargle_oauth_cache = ".secrets")
+
+# Authenticate manually
+gs4_auth()
+
+# If successful, the previous step stores a token file.
+# Check that a file has been created with:
+list.files(".secrets/")
+
+# Check that the non-interactive authentication works by first deauthorizing:
+gs4_deauth()
+
