b3c2afe2d4a91b6e3c7abe13dd81655096ca722a
mauriciogq1 <143091430+mauriciogq1@users.noreply.github.com>
2024-03-10 22:59:25 -0300

    Add files via upload

---
+++ 12.Manipulación_de_datos_con_tidyr/initLesson.R
@@ -0,0 +1,21 @@
+# Code placed in this file fill be executed every time the
+      # lesson is started. Any variables created here will show up in
+      # the user's working directory and thus be accessible to them
+      # throughout the lesson.
+library(dplyr)
+library(tidyr)
+swirl_options(swirl_logging = TRUE)
+library(googlesheets4)
+
+# Set authentication token to be stored in a folder called `.secrets`
+options(gargle_oauth_cache = ".secrets")
+
+# Authenticate manually
+gs4_auth()
+
+# If successful, the previous step stores a token file.
+# Check that a file has been created with:
+list.files(".secrets/")
+
+# Check that the non-interactive authentication works by first deauthorizing:
+gs4_deauth()
