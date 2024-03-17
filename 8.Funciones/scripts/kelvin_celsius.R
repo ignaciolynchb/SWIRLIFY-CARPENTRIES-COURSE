038fd3432540fd575bc7b05cd241bcc000a610b8
ignaciolynchb <ignaciolynchb@gmail.com>
2024-03-17 12:08:41 -0300

    leccion8

---
+++ 8.Funciones/scripts/kelvin_celsius.R
@@ -0,0 +1,8 @@
+#Completa el código de la siguiente función
+#A partir de un valor de temperatura en Kevin (input), debe convertirlo a Celsius y devolverlo (output)
+
+kelvin_a_celsius <- function() { 
+  #Pista: Para convertir de Kelvin a Celsius se debe restar 273.15
+}
+
+#Guarda tu script. Escribe submit() para continuar
--- 8.Funciones/scripts/my_sum.R
+++ 8.Funciones/scripts/my_sum.R
@@ -1,9 +1,11 @@
+#No es necesario que realices cambios en este script, simplemente analizalo con atención
+
 #my_sum es el nombre que recibe la función
-#a y b son los inputs que recibe la función
+#a y b son los inputs que recibe la función. Se encuentran entre paréntesis
 my_sum <- function(a, b) { 
   the_sum <- a + b #aquí se escribe el cuerpo de la función
-#en este caso, se almacena en the_sum el valor resultante de hacer a+b
-  return(the_sum) #por ultimo, se especifica a la función cuál es el output, en este caso, the sum
+                   #en este caso, se almacena en the_sum el valor resultante de hacer a+b
+  return(the_sum) #por ultimo, el return statement especifica a la función cuál es el output, en este caso, the_sum
 }
 
 #Escribe submit() para continuar
