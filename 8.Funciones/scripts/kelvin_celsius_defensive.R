411b28d3da177adf3af2547fb6dbfc11fef9c201
ignaciolynchb <ignaciolynchb@gmail.com>
2024-03-17 12:37:15 -0300

    leccion8

--- 8.Funciones/scripts/kelvin_celsius_defensive.R
+++ 8.Funciones/scripts/kelvin_celsius_defensive.R
@@ -3,11 +3,13 @@
 kelvin_celsius_defensive <- function(kelvin) { 
     #Progreamación defensiva: el primer paso de la funcion es chequear que el output tenga el formato esperado (numerico)
     if (!is.numeric(kelvin)) {
-      stop("kelvin debe ser numerico") #Si no es numerico, se devuelve este mensaje de error
+      stop("kelvin debe ser numerico. Especifique un numero como input") #Si no es numerico, se devuelve este mensaje de error
     }
     celsius <- kelvin - 273.15
     return(celsius) #Si es numerico, retorna el resultado
-  }
 }
+
+#Ponemos a prueba nuestra función
+kelvin_celsius_defensive("uno")
 
 #Escribe submit() para continuar
