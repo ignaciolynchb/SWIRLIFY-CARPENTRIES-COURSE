76aa2c84652c31629fb45cab3dfc800c65d8c6f6
ignaciolynchb <ignaciolynchb@gmail.com>
2024-03-17 12:14:15 -0300

    kelvin_celsius

--- 8.Funciones/scripts/kelvin_celsius.R
+++ 8.Funciones/scripts/kelvin_celsius.R
@@ -1,7 +1,7 @@
 #Completa el código de la siguiente función
 #A partir de un valor de temperatura en Kevin (input), debe convertirlo a Celsius y devolverlo (output)
 
-kelvin_a_celsius <- function() { 
+kelvin_celsius <- function() { 
   #Pista: Para convertir de Kelvin a Celsius se debe restar 273.15
 }
 
