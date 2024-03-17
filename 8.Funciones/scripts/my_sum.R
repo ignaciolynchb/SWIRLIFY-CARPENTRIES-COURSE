1969db880b76b7fbe5264876076b299b4f573a68
ignaciolynchb <ignaciolynchb@gmail.com>
2024-03-17 11:51:16 -0300

    leccion8

---
+++ 8.Funciones/scripts/my_sum.R
@@ -0,0 +1,9 @@
+#my_sum es el nombre que recibe la función
+#a y b son los inputs que recibe la función
+my_sum <- function(a, b) { 
+  the_sum <- a + b #aquí se escribe el cuerpo de la función
+#en este caso, se almacena en the_sum el valor resultante de hacer a+b
+  return(the_sum) #por ultimo, se especifica a la función cuál es el output, en este caso, the sum
+}
+
+#Escribe submit() para continuar
