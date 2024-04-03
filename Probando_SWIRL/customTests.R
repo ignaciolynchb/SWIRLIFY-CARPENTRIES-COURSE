47aa50a1ad9bc1b8bc0aac87a2c217e461705c15
ignaciolynchb <ignaciolynchb@gmail.com>
2024-04-02 22:35:21 -0300

    progress form

--- Probando_SWIRL/customTests.R
+++ Probando_SWIRL/customTests.R
@@ -1,3 +1,122 @@
+notify <- function() {
+  e <- get("e", parent.frame())
+  if(e$val == "No") return(TRUE)
+  
+  good <- FALSE
+  while(!good) {
+    # Get info
+    name <- readline_clean("What is your full name? ")
+    address <- readline_clean("What is the email address of the person you'd like to notify? ")
+    
+    # Repeat back to them
+    message("\nDoes everything look good?\n")
+    message("Your name: ", name, "\n", "Send to: ", address)
+    
+    yn <- select.list(c("Yes", "No"), graphics = FALSE)
+    if(yn == "Yes") good <- TRUE
+  }
+  
+  # Get course and lesson names
+  course_name <- attr(e$les, "course_name")
+  lesson_name <- attr(e$les, "lesson_name")
+  
+  subject <- paste(name, "just completed", course_name, "-", lesson_name)
+  body = ""
+  
+  # Send email
+  swirl:::email(address, subject, body)
+  
+  hrule()
+  message("I just tried to create a new email with the following info:\n")
+  message("To: ", address)
+  message("Subject: ", subject)
+  message("Body: <empty>")
+  
+  message("\nIf it didn't work, you can send the same email manually.")
+  hrule()
+  
+  # Return TRUE to satisfy swirl and return to course menu
+  TRUE
+}
+
+readline_clean <- function(prompt = "") {
+  wrapped <- strwrap(prompt, width = getOption("width") - 2)
+  mes <- stringr::str_c("| ", wrapped, collapse = "\n")
+  message(mes)
+  readline()
+}
+
+hrule <- function() {
+  message("\n", paste0(rep("#", getOption("width") - 2), collapse = ""), "\n")
+}
+notify <- function() {
+  e <- get("e", parent.frame())
+  if(e$val == "No") return(TRUE)
+
+  good <- FALSE
+  while(!good) {
+    # Get info
+    name <- readline_clean("What is your full name? ")
+    address <- readline_clean("What is the email address of the person you'd like to notify? ")
+
+    # Repeat back to them
+    message("\nDoes everything look good?\n")
+    message("Your name: ", name, "\n", "Send to: ", address)
+
+    yn <- select.list(c("Yes", "No"), graphics = FALSE)
+    if(yn == "Yes") good <- TRUE
+  }
+
+  # Get course and lesson names
+  course_name <- attr(e$les, "course_name")
+  lesson_name <- attr(e$les, "lesson_name")
+
+  subject <- paste(name, "just completed", course_name, "-", lesson_name)
+  body = ""
+
+  # Send email
+  swirl:::email(address, subject, body)
+
+  hrule()
+  message("I just tried to create a new email with the following info:\n")
+  message("To: ", address)
+  message("Subject: ", subject)
+  message("Body: <empty>")
+
+  message("\nIf it didn't work, you can send the same email manually.")
+  hrule()
+
+  # Return TRUE to satisfy swirl and return to course menu
+  TRUE
+}
+
+readline_clean <- function(prompt = "") {
+  wrapped <- strwrap(prompt, width = getOption("width") - 2)
+  mes <- stringr::str_c("| ", wrapped, collapse = "\n")
+  message(mes)
+  readline()
+}
+
+hrule <- function() {
+  message("\n", paste0(rep("#", getOption("width") - 2), collapse = ""), "\n")
+}
+
+###################################################
+
+
+# Put custom tests in this file.
+
+# Uncommenting the following line of code will disable
+# auto-detection of new variables and thus prevent swirl from
+# executing every command twice, which can slow things down.
+
+# AUTO_DETECT_NEWVAR <- FALSE
+
+# However, this means that you should detect user-created
+# variables when appropriate. The answer test, creates_new_var()
+# can be used for for the purpose, but it also re-evaluates the
+# expression which the user entered, so care must be taken.
+
 # Get the swirl state
 getState <- function(){
   # Whenever swirl is running, its callback is at the top of its call stack.
@@ -14,7 +133,7 @@ submit_log <- function(){
   
   # Please edit the link below
   pre_fill_link <- "https://docs.google.com/forms/d/e/1FAIpQLSdB3fmHBC9x-5vti9eCM92WsqTdwoeSn3Xptod951OHFF8m_Q/viewform?usp=pp_url&entry.2015965411=%5BProbando_SWIRL%5D&entry.823771670"
-  
+
   # Do not edit the code below
   if(!grepl("=$", pre_fill_link)){
     pre_fill_link <- paste0(pre_fill_link, "=")
@@ -34,6 +153,8 @@ submit_log <- function(){
                         skipped = p(log_$skipped, nrow_, NA),
                         datetime = p(log_$datetime, nrow_, NA),
                         stringsAsFactors = FALSE)
-  if(length(log_tbl)!=0){write.csv(log_tbl, file = temp, row.names = FALSE)}
+  write.csv(log_tbl, file = temp, row.names = FALSE)
   encoded_log <- base64encode(temp)
+  browseURL(paste0(pre_fill_link, encoded_log))
+  
 }# So swirl does not repeat execution of commands
